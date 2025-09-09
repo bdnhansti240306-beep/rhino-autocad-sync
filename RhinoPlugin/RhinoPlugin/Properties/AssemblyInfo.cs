f1eb341f7a84bebf9640fd5957f9f01e00cbfb71
bdnhansti240306-beep <bdnhansti240306@gmail.com>
2025-09-09 12:41:45 +0700

    Create AssemblyInfo.cs

---
+++ RhinoPlugin/RhinoPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Rhino.PlugIns;
+
+[assembly: AssemblyTitle("RhinoToAutoCADSync")]
+[assembly: AssemblyDescription("Export Rhino objects to AutoCAD with file selection")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
+[assembly: Guid("12345678-1234-1234-1234-123456789abc")]
+
+[assembly: PlugInDescription(DescriptionType.Address, "")]
+[assembly: PlugInDescription(DescriptionType.Country, "Vietnam")]
+[assembly: PlugInDescription(DescriptionType.Email, "")]
+[assembly: PlugInDescription(DescriptionType.Phone, "")]
+[assembly: PlugInDescription(DescriptionType.Fax, "")]
+[assembly: PlugInDescription(DescriptionType.Organization, "")]
+[assembly: PlugInDescription(DescriptionType.UpdateUrl, "")]
+[assembly: PlugInDescription(DescriptionType.WebSite, "")]
